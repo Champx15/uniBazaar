769a315e022a9447af9c4627119b0b3644f98ccc
champx15 <munawarxpower@gmail.com>
2026-03-24 22:48:03 +0530

    config change

--- Frontend/src/conf/conf.js
+++ Frontend/src/conf/conf.js
@@ -1,6 +1,5 @@
 const conf = {
-  // apiBase: import.meta.env.VITE_API_BASE_URL,
-  apiBase: `${window.location.protocol}//${window.location.hostname}:8080/api/v1`,
+  apiBase: import.meta.env.VITE_API_BASE_URL,
   cloudName: import.meta.env.VITE_CLOUDINARY_CLOUD_NAME ,
   uploadPreset: import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET,
   cloudinaryApi:import.meta.env.VITE_CLOUDINARY_API_KEY
